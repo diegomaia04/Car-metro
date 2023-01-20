5485e6b40497799304c97cd3ebba5d3f5279ea4c
Diego Maia Fernandes <93396521+diegomaia04@users.noreply.github.com>
2023-01-20 20:39:17 -0300

    Add files via upload

---
+++ Banco/banco de dados-Carômetro.sql
@@ -0,0 +1,19 @@
+/**
+Mini projeto - Carômetro
+@autor - Diego Maia Fernandes
+*/
+create database carometro;
+show databases;
+use carometro;
+
+-- blob, mediumblob, longblob (tipos de dados que aceitam arquivos no formato binario)
+create table alunos (
+  id int primary key auto_increment,
+  nome varchar(50) not null,
+  foto longblob
+  );
+  
+  describe alunos; 
+  
+  select * from alunos;
+  select * from alunos where nome = "andy";
